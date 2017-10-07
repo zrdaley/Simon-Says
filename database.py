9f289595e077064c07d1838a1505cc0a5281f08b
Zenara Daley <zrdaley@gmail.com>
2017-10-07 10:42:42 -0700

    added authentication

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 # stdlib
-from flask import current_app
+import os
+import hashlib
+import json
 from random import randint
 
 # 3rd party
+from flask import current_app
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON, UUID
 from sqlalchemy.orm.attributes import flag_modified
@@ -21,11 +24,45 @@ class Players(db.Model):
         self.high_score = hs
 
 
+class Accounts(db.Model):
+    user_id = db.Column(db.String(100), primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+
+    def __init__(self, uid, un, pw):
+        self.user_id = uid,
+        self.username = un
+        self.password = pw
+
+
 class Database(object):
 
-    def create_new_user(self, username, user_id):
+    def _hash_password(self, password):
+        # Salt and hash password for storage
+        h = hashlib.md5(password + os.environ['SALT'])
+        password_hash = h.hexdigest()
+        return password_hash
+
+    def account_exists(self, username):
+        account = Accounts.query.filter_by(username=username).first()
+        if account:
+            return True
+        return False
+
+    def authenticate_user(self, username, password):
+        account = Accounts.query.filter_by(username=username).first()
+        password_hash = self._hash_password(password)
+        if password_hash == str(account.password):
+            return account.user_id
+        return None
+
+    def create_new_user(self, username, user_id, password):
+        # Create new user and account
         user = Players(user_id, username, None)
+        account = Accounts(user_id, username, self._hash_password(password))
+       
         db.session.add(user)
+        db.session.add(account)
         db.session.commit()
 
     def get_high_score(self, user_id):
@@ -55,9 +92,3 @@ class Database(object):
             db.session.commit()
             return user.high_score
         return None
-
-    def username_exists(self, username):
-        user = Players.query.filter_by(username=username).first()
-        if user:
-            return True
-        return False
--- routes/index.py
+++ routes/index.py
@@ -1,5 +1,6 @@
 # stdlib
 import uuid
+import re
 
 # 3rd party
 from flask import \
@@ -17,6 +18,7 @@ from database import Database
 routes = Blueprint('index_routes', __name__, template_folder='templates')
 db = Database()
 MAX_USER_LENGTH = 50
+PASSWORD_RE = '[A-Za-z0-9@#$%^&+=]{6,100}'
 
 
 @routes.route("/")
@@ -32,43 +34,56 @@ def index():
 @routes.route("/create_account")
 def create():
 	username = request.args.get('username')
+	password = request.args.get('password')
 
 	# Filter username
 	if not username:
 		return redirect(url_for('index_routes.index', create_error="Please enter a valid username"))
 	if len(username) > MAX_USER_LENGTH:
-		return redirect(url_for('index_routes.index', create_error="Username cannot exceed 100 characters"))
+		return redirect(url_for('index_routes.index', create_error="Username cannot exceed 50 characters"))
 	if not username.isalnum():
 		return redirect(url_for('index_routes.index', create_error="Usernames can only contain a-z, A-Z, or 0-9"))
-
 	# Check if username exists
-	if db.username_exists(username):
+	if db.account_exists(username):
 		return redirect(url_for('index_routes.index', create_error="The username '{}' is already taken".format(username)))
+
+	# Filter password
+	if not re.match(PASSWORD_RE, password):
+		return redirect(url_for('index_routes.index', create_error="Invalid password"))
 	
 	# Create user ID, add user to db, set cookie for gameplay, redirect to game
 	user_id = uuid.uuid4().hex
 	session['user_id'] = user_id 
-	db.create_new_user(username, str(user_id))
+	db.create_new_user(username, str(user_id), password)
 	return redirect(url_for('play_routes.simon_says'))
 
 @routes.route("/login")
 def login():
 	username = request.args.get('username')
+	password = request.args.get('password')
 
 	# Filter username
 	if not username:
 		return redirect(url_for('index_routes.index', login_error="Please enter a valid username"))
 	if len(username) > MAX_USER_LENGTH:
-		return redirect(url_for('index_routes.index', login_error="Username cannot exceed 100 characters"))
+		return redirect(url_for('index_routes.index', login_error="Username cannot exceed 50 characters"))
 	if not username.isalnum():
 		return redirect(url_for('index_routes.index', login_error="Usernames can only contain a-z, A-Z, or 0-9"))
 
 	# Check if username exists
-	if not db.username_exists(username):
+	if not db.account_exists(username):
 		return redirect(url_for('index_routes.index', login_error="The username '{}' does not exist".format(username)))
+
+	# Filter password
+	if not re.match(PASSWORD_RE, password):
+		return redirect(url_for('index_routes.index', login_error="Invalid password"))
+
+	# Authenticate
+	user_id = db.authenticate_user(username, password)
+	if not user_id:
+		return redirect(url_for('index_routes.index', login_error="Invalid credentials"))
 	
 	# Set cookie for gameplay, redirect to game
-	user_id = db.get_user_id(username)
 	session['user_id'] = user_id 
 	return redirect(url_for('play_routes.simon_says'))
 
