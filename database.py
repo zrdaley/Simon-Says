b60bf332e153972667e2a1f84ab0a5e1318aacf4
Zenara Daley <zrdaley@gmail.com>
2017-10-07 12:55:25 -0700

    fixed database

--- database.py
+++ database.py
@@ -13,26 +13,17 @@ from sqlalchemy.orm.attributes import flag_modified
 db = SQLAlchemy()
 
 
-class Players(db.Model):
-    user_id = db.Column(db.String(100), primary_key=True)
-    username = db.Column(db.String(50))
-    high_score = db.Column(db.Integer)
-
-    def __init__(self, uid, un, hs):
-        self.user_id = uid,
-        self.username = un
-        self.high_score = hs
-
-
 class Accounts(db.Model):
     user_id = db.Column(db.String(100), primary_key=True)
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
+    high_score = db.Column(db.Integer)
 
-    def __init__(self, uid, un, pw):
+    def __init__(self, uid, un, pw, hs):
         self.user_id = uid,
         self.username = un
         self.password = pw
+        self.high_score = hs
 
 
 class Database(object):
@@ -57,36 +48,32 @@ class Database(object):
         return None
 
     def create_new_user(self, username, user_id, password):
-        # Create new user and account
-        user = Players(user_id, username, None)
-        account = Accounts(user_id, username, self._hash_password(password))
-       
-        db.session.add(user)
+        account = Accounts(user_id, username, self._hash_password(password), None)
         db.session.add(account)
         db.session.commit()
 
     def get_high_score(self, user_id):
-        user = Players.query.filter_by(user_id=user_id).first()
+        user = Accounts.query.filter_by(user_id=user_id).first()
         return user.high_score
 
     def get_top_users(self):
-        top_users = Players.query.filter(Players.high_score != None).order_by(Players.high_score).limit(3)
+        top_users = Accounts.query.filter(Accounts.high_score != None).order_by(Accounts.high_score).limit(3)
         return top_users
 
     def get_username(self, user_id):
-        user = Players.query.filter_by(user_id=user_id).first()
+        user = Accounts.query.filter_by(user_id=user_id).first()
         if user:
             return user.username
         return None
 
     def get_user_id(self, username):
-        user = Players.query.filter_by(username=username).first()
+        user = Accounts.query.filter_by(username=username).first()
         if user:
             return user.user_id
         return None
 
     def update_high_score(self, user_id, score):
-        user = Players.query.filter_by(user_id=user_id).first()
+        user = Accounts.query.filter_by(user_id=user_id).first()
         if user.high_score is None or score > user.high_score:
             user.high_score = score
             db.session.commit()
